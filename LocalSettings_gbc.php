63b5d5af062aad5d77d4693b9defde1f711d1942
Brian Alano <balano@umail.iu.edu>
2015-01-26 21:02:05 -0500

    Update LocalSettings_gbc.php
    remove "the" from the Sitename. Would like to change the favicon. Where to upload it?

--- LocalSettings_gbc.php
+++ LocalSettings_gbc.php
@@ -1,11 +1,11 @@
 <?php
 
-$wgSitename = "Great Ball Contraption Wiki";
+$wgSitename = "the Great Ball Contraption wiki";
 $wgMetaNamespace = "GBC";
 $wgPasswordSender = "gbc";
-$wgPasswordSenderName = "Great Ball Contraption Wiki";
+$wgPasswordSenderName = "the Great Ball Contraption wiki";
 
 //$wgLogo = "http://images.brickimedia.org/d/df/BrickiCustomsLogo.png/160px-BrickiCustomsLogo.png";
-$wgFavicon = "http://images.brickimedia.org/favicons/favicon.ico";
+$wgFavicon = "http://images.brickimedia.org/favicons/gbc.ico";
 
 $wgLanguageCode = "en";
